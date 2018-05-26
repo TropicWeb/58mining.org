629e5583f91ac69b40b360ef60ac2fcf9a415d7b
amomymoderCanaima <amomymoder@gmail.com>
2018-05-25 21:24:56 -0400

    Main.php actualizado y responsive

--- views/main.php
+++ views/main.php
@@ -39,7 +39,7 @@
         <li class="active"><a class="itemMenu" href="#">Home<span class="sr-only">(current)</span></a></li>
         <li><a class="itemMenu" href="#">Us</a></li>
         <li><a class="itemMenu" href="#">Statics</a></li>
-        <li><a class="itemMenu" href="calculator">Calculator</a></li>
+        <li><a class="itemMenu" href="#">Calculator</a></li>        
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
@@ -53,7 +53,7 @@
     <section class="col-md-12 col-xs-12 col-sm-12 col-lg-12">
       <img src="Images/oro.jpg" class="img-responsive">
     </section>
-  </div>
+  </div>  
 </section>
 <!--Fin S2-->
 
@@ -64,8 +64,8 @@
     <div class="col-md-4">
       <p class="texthome text-center martex">En <b>+58mining</b> nos apasiona la tecnología block chain, su innovación en la integridad de la información no tiene fronteras, generando mayor alcance global que logra cambios en el mundo</p>
     </div>
-    <div class="col-md-4"></div>
-  </div>
+    <div class="col-md-4"></div>  
+  </div>     
 </section>
 <!--Fin S3-->
 
@@ -84,30 +84,86 @@
       <br>
       <br>
       <br>
-      <br>
-      <b>Blockchain</b> es una tecnología que permite la transferencia de datos digitales codificados de una manera completamente <b>segura</b>. Es vista como el libro de asientos de contabilidad de una empresa en donde se registran todas las entradas y todas las salidas de dinero. La gran innovación es que esta transferencia no requiere de un intermediario centralizado que identifique y certifique la información, sino que la validación de las transacciones está distribuida en múltiples nodos independientes, conocidos popularmente como mineros. Los mineros validan la información a través del protocolo de datos y luego la registran en el “libro de asientos”. <b>Por ejemplo</b>, en una transacción financiera, el minero valida la tenencia de los fondos, la autorización del uso de ellos y la acreditación de los fondos, para luego registrar esa transacción.
-      <br>
+      <b>Blockchain</b> es una tecnología que permite la transferencia de datos digitales codificados de una manera completamente <b>segura</b>. Es vista como el libro de asientos de contabilidad de una empresa en donde se registran todas las entradas y todas las salidas de dinero. La gran innovación es que esta transferencia no requiere de un intermediario centralizado que identifique y certifique la información, sino que la validación de las transacciones está distribuida en múltiples nodos independientes, conocidos popularmente como mineros. Los mineros validan la información a través del protocolo de datos y luego la registran en el “libro de asientos”. <b>Por ejemplo</b>, en una transacción financiera, el minero valida la tenencia de los fondos, la autorización del uso de ellos y la acreditación de los fondos, para luego registrar esa transacción. 
       <br>
       <br>
       <br>
       El Financial Times, realizó un esquema ilustrativo donde se puede apreciar el funcionamiento del blockchain con el envío de dinero entre A y B.</p>
-    </section>
+    </section>  
     <section class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
         <img src="Images/cadena.png" class="img-responsive">
-    </section>
-  </div>
+    </section> 
+  </div>     
 </section>
 <!--Fin S4-->
 
-<!--Sección 4 Final-->
-<footer class="container-fluid section5">
+<!--Sección 5 Imágen2-->
+<section class="section5 container-fluid">
+  <div class="row">
+    <section class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
+      <img src="Images/expli.jpg" class="img-responsive">
+    </section>
+  </div>  
+</section>
+<!--Fin S5-->
+
+<!--Seccion 6 Wallets-->
+<section class="container-fluid section6">
+  <div class="row">
+    <section class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <br>
+      <br>
+      <h3 class=" text-center blockchain"><b>¿Qué son los Wallets?</b></h3>
+      <br>
+      <br>
+      <p class="hometex text-center">Existen elementos realmente importantes en el mundo de las criptomonedas, uno de ellos sin duda alguna son <b>los wallets</b>, pero ¿Qué es un wallet?, un wallet, monedero o cartera en español, es el sitio donde se almacenan las criptomonedas, en el sistema monetario que conocemos es equivalente a la cuenta bancaria.
+      <br>
+      <br>
+      Los monederos están integrados por dos llaves conformadas por una secuencia de números y letras que determinan el monedero, la llave pública y la llave privada:
+      <b>La llave pública</b>, como su nombre lo especifica, es de uso público y es la dirección que se facilita para poder recibir transferencias.
+      <b>La llave privada</b>, es una especie de contraseña que autoriza el acceso al monedero y es necesaria para la liberación de los fondos contenidos en el mismo. Es importante destacar que la persona que conozca la llave privada, incluso sin conocer la llave pública, tiene el control absoluto de sus criptoactivos, por esto es de suma importancia guardarla en un sitio seguro y dependerá de las características del monedero la seguridad de la misma.
+      <br>
+      <br>
+      Existen múltiples clasificaciones de los monederos, sin embargo, generalmente se clasifican en dos tipos:</p>
+      <br>
+    </section>  
+  </div> 
+  <div class="row">
+    <section class="col-md-1"></section>
+    <section class="col-md-4">
+      <div class="tips center-block">
+        <p class="text-center test"><b>Monederos calientes (wallets online):</b> son monederos bastante sencillos de utilizar, los usuarios deben registrarse con un login, normalmente un correo electrónico, y una contraseña para luego con un par de clic crear su cartera en la web. Al estar en servidores online, estos sitios están bajo los constantes ataques de hackers que buscan conseguir las llaves privadas, y recuerden, el que tenga las llaves privadas tiene el control de los activos. Algunos ejemplos de estos monederos son: Uphold, Xapo, Coinbase Wallet, Blockchain, entre otros.</p> 
+      </div>        
+    </section>
+    <section class="col-md-2"></section>
+    <section class="col-md-4">
+      <div class="tips center-block">
+        <p class="text-center"><b>Monederos fríos (wallets offline):</b> son monederos que almacenan la información fuera de la web, como un papel, un pendrive o un software de computadora. En este tipo de monederos la clave privada está en el poder del propietario, siendo este el responsable de sus criptoactivos, para realizar transferencias se debe cargar dicha llave privada en alguna plataforma especializada, sin embargo, en el caso de perder la llave privada, no existe forma de recuperar los fondos. Algunos ejemplos de este tipo de monederos son: dispositivos Ledger y Trezor, página web MyEtherWallet.com que genera un código en papel (Paper Wallet).</p>  
+      </div>          
+    </section>
+    <section class="col-md-1"></section>
+  </div>    
+  <div class="row">
+    <section class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <br>
+      <p class="hometex text-center">En relación a la clasificación de los monederos, es importante aclarar que los sitios web como Poloniex, Bitfinex, Bittrex, Kraken, entre muchos otros, no son monederos. Estos sitios son casas de cambios (exchanges) de criptoactivos que manejan múltiples monedas. Estos sitios prestan el servicio de monederos, para poder realizar depósitos y retiros, sin embargo, su principal función no es el almacenamiento de las criptomonedas sino el intercambio de ellas. Por lo tanto, no es recomendable almacenar las criptomonedas en estos sitios.
+      Se recomienda,  almacenar en wallets offline la mayor cantidad de criptomonedas, estos son más seguros, utilizar casas de cambios en el caso de querer intercambiar criptoactivos y wallets online si requiere realizar pagos frecuentes.</p>
+      <br>
+      <br>      
+    </section>
+  </div> 
+</section>
+<!--Fin S6-->
+
+<!--Sección  Final-->
+<footer class="container-fluid sectionF">
   <div class="row">
     <article class="col-xs-12 col-md-4 col-lg-4">
       <ul class="list-inline text-center fot">
         <li><a href="#" class="menuF" >Home</a></li>
         <li><a href="#" class="menuF" >Us</a></li>
         <li><a href="#" class="menuF" >Statics</a></li>
-        <li><a href="calculator" class="menuF" >Calculator</a></li>
+        <li><a href="#" class="menuF" >Calculator</a></li>
       </ul>
     </article>
     <article class="col-xs-12 col-md-4 col-lg-4"></article>
@@ -116,10 +172,10 @@
     </article>
   </div>
 </footer>
-<!--Fin S5-->
+<!--Fin SF-->
 
   <script type="text/javascript" src="JS/jquery.min.js"></script>
   <script type="text/javascript" src="JS/bootstrap.min.js"></script>
-
+ 
   </body>
 </html>
